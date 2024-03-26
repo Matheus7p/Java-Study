81b4963fb7053419f69fb9ddc70565f1e73da434
Matheus7p <matheus.costa.7p@gmail.com>
2024-03-25 22:11:40 -0300

    Switching player each turn

--- projects/chess-system/src/application/UI.java
+++ projects/chess-system/src/application/UI.java
@@ -1,9 +1,11 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
 
+import javax.swing.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -55,6 +57,13 @@ public class UI {
         System.out.print("  a b c d e f g h");
     }
 
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
+
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
         for (int i=0; i<pieces.length; i++){
             System.out.print((8 - i) + " ");
@@ -74,7 +83,7 @@ public class UI {
             System.out.print("-" + ANSI_RESET);
         }
         else {
-            if (piece.getColor() == Color.RED) {
+            if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_RED + piece + ANSI_RESET);
             }
             else {
