424ecdc6a6f7909f611681ab77cd2be8be176175
Matheus7p <matheus.costa.7p@gmail.com>
2024-03-25 23:40:49 -0300

    CheckMate logic

--- projects/chess-system/src/application/UI.java
+++ projects/chess-system/src/application/UI.java
@@ -6,6 +6,7 @@ import chess.ChessPosition;
 import chess.Color;
 
 import javax.swing.*;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -66,10 +67,15 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-
-        if(chessMatch.getCheck()){
-            System.out.println("CHECK!");
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.println("CHECK!");
+            }
+        }
+        else {
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
         }
     }
 
